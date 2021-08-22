1c3c88f597159b12afc64bbee417383785a69d05
Kartse <karmat359@gmail.com>
2021-08-22 13:02:05 +0545

    added regex

--- app.js
+++ app.js
@@ -6,6 +6,9 @@ const password = form["password"]
 const submitbtn = form["submit"]
 const checkbox = form["checkbox"]
 const checkboxError = document.querySelector(".checkbox-error")
+// regular expression to validate the charactres in Email and Pasword
+const reEmail = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+const rePassword = /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#\$%\^&\*])(?=.{8,})/
 let type = ""
 
 // <-------- function for error message and icon ----------->
@@ -18,14 +21,14 @@ function errorMessage(inputType,text) {
     iconError.style.display = "block"
     if (inputType === email) {
         email.placeholder = "email@example/com"
+        inputType.style.borderColor = "var(--red)"
         email.classList.add("red-email")
         email.focus()
     } else {
         inputType.placeholder = ""
         inputType.style.borderColor = "var(--red)"
         inputType.focus()
- }
-    
+    }
 }
 
 // <------------- function for clear the error message and icon ----------->
@@ -39,34 +42,14 @@ function clearErrorMessage(inputType) {
     inputType.style.borderColor = "rgba(226, 226, 226, .5)"
 }
 
-// <-------- email verification --------->
-function emailVerification() {
-    let flag = 0;
-    type = "email"
-    let errorText = "*not a valid Email"
-    let strs = email.value
-    for (str of strs) {
-        if (str === "@" || str === ".")
-            ++flag
-    }
-    if (flag >= 2)
-        clearErrorMessage(email)
-    else {
-        errorMessage(email,errorText)
-        return false
-    }
-}
 
-// <--------- password verification ---------->
-function passwordVerification() {
-    
-}
+// <--------------- function for custome form validation --------------->
 function formValidation() {
     // <----------- First Name validation ------->
     if (fname.value === "") {
         type = "fname"
         let errorText = "*First Name cannot be empty!"
-        errorMessage(fname,errorText)
+        errorMessage(fname, errorText)
         return false
     } else {
         type = "fname"
@@ -76,37 +59,42 @@ function formValidation() {
     if (lname.value === "") {
         type = "lname"
         let errorText = "*List Name cannot be empty!"
-        errorMessage(lname,errorText)
+        errorMessage(lname, errorText)
         return false
     } else {
         type = "lname"
         clearErrorMessage(lname)
     }
     // <------------ Email validation -------------->
-    if (email.value === "") {
+    if (reEmail.test(email.value)) {
         type = "email"
-        let errorText = "*Email cannot be empty!"
-        errorMessage(email,errorText)
-        return false
-    } else {
-        emailVerification()
+        clearErrorMessage(email)
     }
+    else {
+        type = "email"
+        let errorText = "*not a valid Email"
+            errorMessage(email, errorText)
+            return false
+        }
+    
     // <----------- Password validation ----------->
-    if (password.value === "") {
-        type = "password"
-        let errorText = "*Password cannot be empty!"
-        errorMessage(password,errorText)
-        return false
-    } else {
-        type = "password"
-        clearErrorMessage(password)
-    }
-// <------------ checkbox validation ------------>
+        if (rePassword.test(password.value)) {
+            type = "password"
+            clearErrorMessage(password)
+        }
+        else {
+            type = "password"
+            let errorText = "*Weak password"
+            errorMessage(password, errorText)
+            return false
+        }
+    
+    // <------------ checkbox validation ------------>
     if (checkbox.checked == false) {
         checkboxError.style.visibility = "visible"
         return false
-    } else{
-       checkboxError.style.visibility = "hidden"
+    } else {
+        checkboxError.style.visibility = "hidden"
     }
 }
 
